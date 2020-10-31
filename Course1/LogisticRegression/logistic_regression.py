75438e0366a63287dc2c87d83258501528a40f7b
lexiaoyuan <2775886918@qq.com>
2020-10-31 18:00:09 +0800

    update

--- Course1/LogisticRegression/logistic_regression.py
+++ Course1/LogisticRegression/logistic_regression.py
@@ -22,6 +22,8 @@ m_train ：训练集里图片的数量。
 m_test ：测试集里图片的数量。
 num_px ： 训练、测试集里面的图片的宽度和高度（均为64x64）。
 '''
+print(train_set_y)
+print(train_set_y.shape)
 m_train = train_set_y.shape[1]  # 训练集里图片的数量。
 m_test = test_set_y.shape[1]  # 测试集里图片的数量。
 num_px = train_set_x_orig.shape[1]  # 训练、测试集里面的图片的宽度和高度（均为64x64）。
@@ -42,8 +44,9 @@ train_set_x_flatten = train_set_x_orig.reshape(train_set_x_orig.shape[0], -1).T
 # 将测试集的维度降低并转置。
 test_set_x_flatten = test_set_x_orig.reshape(test_set_x_orig.shape[0], -1).T
 
-# 降维之后，每一列表示一张图片，训练集共有209列，64*64*3=12288行
+print(train_set_x_flatten)
 
+# 降维之后，每一列表示一张图片，训练集共有209列，64*64*3=12288行
 print("训练集降维最后的维度： " + str(train_set_x_flatten.shape))  # (12288, 209)
 print("训练集_标签的维数 : " + str(train_set_y.shape))  # (1, 209)
 print("测试集降维之后的维度: " + str(test_set_x_flatten.shape))  # (12288, 50)
@@ -52,6 +55,8 @@ print("测试集_标签的维数 : " + str(test_set_y.shape))  # (1, 50)
 # 标准化我们的数据集
 train_set_x = train_set_x_flatten / 255
 test_set_x = test_set_x_flatten / 255
+
+print(train_set_x)
 
 '''
 建立神经网络的主要步骤是：
@@ -106,39 +111,41 @@ def initialize_with_zeros(dim):
             b  - 初始化的标量（对应于偏差）
     """
     # w = np.zeros(shape=(dim, 1))
-    w = np.random.randn(dim, 1) * 0.008
+    w = np.random.randn(dim, 1) * 0.008  # 返回一个12288*1的数组，值为从均值为0，方差为1的正态分布中随机采样得到的一组数。
     b = 0
     # 使用断言来确保我要的数据是正确的
-    assert (w.shape == (dim, 1))  # w的维度是(dim,1)
+    assert (w.shape == (dim, 1))  # w的维度是(12288,1)
     assert (isinstance(b, float) or isinstance(b, int))  # b的类型是float或者是int
 
     return w, b
 
 
-# 计算成本函数及其渐变的函数
+# 计算成本函数和损失梯度
 def propagate(w, b, X, Y):
     """
     实现前向和后向传播的成本函数及其梯度。
     参数：
-        w  - 权重，大小不等的数组（num_px * num_px * 3，1）
+        w  - 权重，大小不等的数组（12288，1）
         b  - 偏差，一个标量
-        X  - 矩阵类型为（num_px * num_px * 3，训练数量）
-        Y  - 真正的“标签”矢量（如果非猫则为0，如果是猫则为1），矩阵维度为(1,训练数据数量)
+        X  - 矩阵类型为（12288，209）
+        Y  - 真正的“标签”矢量（如果非猫则为0，如果是猫则为1），矩阵维度为(1, 209)
 
     返回：
         cost- 逻辑回归的负对数似然成本
         dw  - 相对于w的损失梯度，因此与w维度相同
         db  - 相对于b的损失梯度，因此与b维度相同
     """
-    m = X.shape[1]
+    m = X.shape[1]  # 12288
 
     # 正向传播
     A = sigmoid(np.dot(w.T, X) + b)  # 计算激活值，请参考公式2。
+    # print(A.shape)  # (1, 209)
     cost = (- 1 / m) * np.sum(Y * np.log(A) + (1 - Y)
                               * (np.log(1 - A)))  # 计算成本，请参考公式3和4。
-
+    # print(cost.shape) # ()
     # 反向传播
     dw = (1 / m) * np.dot(X, (A - Y).T)  # 请参考视频中的偏导公式。
+    # print(dw.shape)  # (12288, 1)
     db = (1 / m) * np.sum(A - Y)  # 请参考视频中的偏导公式。
 
     # 使用断言确保我的数据是正确的
@@ -166,15 +173,15 @@ print("db = " + str(grads["db"]))
 print("cost = " + str(cost))
 
 
-# 使用渐变下降更新参数
+# 使用梯度下降更新参数
 def optimize(w, b, X, Y, num_iterations, learning_rate, print_cost=False):
     """
     此函数通过运行梯度下降算法来优化w和b
 
     参数：
-        w  - 权重，大小不等的数组（num_px * num_px * 3，1）
+        w  - 权重，大小不等的数组（12288，1）
         b  - 偏差，一个标量
-        X  - 维度为（num_px * num_px * 3，训练数据的数量）的数组。
+        X  - 维度为（12288，209）的数组。
         Y  - 真正的“标签”矢量（如果非猫则为0，如果是猫则为1），矩阵维度为(1,训练数据的数量)
         num_iterations  - 优化循环的迭代次数
         learning_rate  - 梯度下降更新规则的学习率
@@ -245,24 +252,26 @@ def predict(w, b, X):
     使用学习逻辑回归参数logistic （w，b）预测标签是0还是1，
 
     参数：
-        w  - 权重，大小不等的数组（num_px * num_px * 3，1）
+        w  - 权重，大小不等的数组（12288，1）
         b  - 偏差，一个标量
-        X  - 维度为（num_px * num_px * 3，训练数据的数量）的数据
+        X  - 维度为（12288，209）的数据
 
     返回：
         Y_prediction  - 包含X中所有图片的所有预测【0 | 1】的一个numpy数组（向量）
 
     """
 
-    m = X.shape[1]  # 图片的数量
+    m = X.shape[1]  # 图片的数量 209
     Y_prediction = np.zeros((1, m))
     w = w.reshape(X.shape[0], 1)
+    # print(w.shape)  # (12288, 1)
 
     # 计算预测猫在图片中出现的概率
     A = sigmoid(np.dot(w.T, X) + b)
+    # print(A.shape)  # (1, 209)
     for i in range(A.shape[1]):
         # 将概率a [0，i]转换为实际预测p [0，i]
-        Y_prediction[0, i] = 1 if A[0, i] > 0.5 else 0
+        Y_prediction[0, i] = 1 if A[0, i] > 0.5 else 0  # python中类似三元表达式的实现方法
     # 使用断言
     assert (Y_prediction.shape == (1, m))
 
@@ -282,8 +291,8 @@ def model(X_train, Y_train, X_test, Y_test, num_iterations=2000, learning_rate=0
     通过调用之前实现的函数来构建逻辑回归模型
 
     参数：
-        X_train  - numpy的数组,维度为（num_px * num_px * 3，m_train）的训练集
-        Y_train  - numpy的数组,维度为（1，m_train）（矢量）的训练标签集
+        X_train  - numpy的数组,维度为（12288，209）的训练集
+        Y_train  - numpy的数组,维度为（1，209）（矢量）的训练标签集
         X_test   - numpy的数组,维度为（num_px * num_px * 3，m_test）的测试集
         Y_test   - numpy的数组,维度为（1，m_test）的（向量）的测试标签集
         num_iterations  - 表示用于优化参数的迭代次数的超参数
--- Course1/LogisticRegression/lr_utils.py
+++ Course1/LogisticRegression/lr_utils.py
@@ -11,17 +11,33 @@ classes ： 保存的是以bytes类型保存的两个字符串数据，数据为
 
 
 def load_dataset():
+    # 加载训练集的数据
     train_dataset = h5py.File('datasets/train_catvnoncat.h5', "r")
-    train_set_x_orig = np.array(train_dataset["train_set_x"][:])  # your train set features
-    train_set_y_orig = np.array(train_dataset["train_set_y"][:])  # your train set labels
+    print(train_dataset)  # <HDF5 file "train_catvnoncat.h5" (mode r)>
+    train_set_x_orig = np.array(train_dataset["train_set_x"][:])
+    print(train_set_x_orig)  # 209张图片，每张图片有64*64个点，每个点有3个通道，每个通道的值是对应颜色的十进制的值。
+    print(train_set_x_orig.shape)  # （209, 64, 64, 3)
+    train_set_y_orig = np.array(
+        train_dataset["train_set_y"][:])  # your train set labels
+    print(train_set_y_orig)  # 有209个值的数组，每个值对应每张图片的分类值
+    print(train_set_y_orig.shape)  # (209,)
 
+    # 加载测试集的数据
     test_dataset = h5py.File('datasets/test_catvnoncat.h5', "r")
-    test_set_x_orig = np.array(test_dataset["test_set_x"][:])  # your test set features
-    test_set_y_orig = np.array(test_dataset["test_set_y"][:])  # your test set labels
+    test_set_x_orig = np.array(test_dataset["test_set_x"][:])
+    test_set_y_orig = np.array(
+        test_dataset["test_set_y"][:])  # your test set labels
 
     classes = np.array(test_dataset["list_classes"][:])  # the list of classes
+    print(classes)  # [b'non-cat' b'cat']
 
+    # 转成1*209的矩阵
     train_set_y_orig = train_set_y_orig.reshape((1, train_set_y_orig.shape[0]))
+    print(train_set_y_orig)
+    print(train_set_y_orig.shape)  # (1, 209)
     test_set_y_orig = test_set_y_orig.reshape((1, test_set_y_orig.shape[0]))
 
     return train_set_x_orig, train_set_y_orig, test_set_x_orig, test_set_y_orig, classes
+
+
+# load_dataset()
